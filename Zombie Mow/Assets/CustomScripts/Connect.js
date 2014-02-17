a3ddff7b0ec137daf2d66b1384cc8e396ec392e0
Ammie <knapiflable@hotmail.com>
2014-02-17 13:30:59 -0600

    Updated unity stuff
    I added the network test built file so that you can see how it runs
    easier. The start game button has nothing set to it yet. but the test
    servers button works.

--- Zombie Mow/Assets/CustomScripts/Connect.js
+++ Zombie Mow/Assets/CustomScripts/Connect.js
@@ -6,61 +6,67 @@ var log: String = "";
 var myMessage : String = "";
 var chatting : boolean = false;
 
+
+
 function OnGUI () {
-if(Network.peerType == NetworkPeerType.Disconnected) {
-	 GUILayout.BeginHorizontal();
-	 ipAddress=GUILayout.TextField(ipAddress);
-	 GUILayout.Label("IP ADDRESS");
-	 GUILayout.EndHorizontal();
+	
+	if(Network.peerType == NetworkPeerType.Disconnected) {
+		 GUILayout.BeginHorizontal();
+		 ipAddress=GUILayout.TextField(ipAddress);
+		 GUILayout.Label("IP ADDRESS");
+		 GUILayout.EndHorizontal();
+		 
+		 GUILayout.BeginHorizontal();
+		 var tempPort : String; 
+		 tempPort = GUILayout.TextField(port.ToString());
+		 port = parseInt (tempPort);
+		 GUILayout.Label ("PORT");
+		 GUILayout.EndHorizontal();
+		 
+		 if (GUILayout.Button ("CONNECT")) {
+		 
+		 	print ("connecting...");
+		  	Network.Instantiate (playerGO, transform.position, transform.rotation, 0);
+		 	Network.Connect (ipAddress, port);
+		 	for (var i : GameObject in FindObjectsOfType(GameObject)){
+	 			i.BroadcastMessage("OnLoadNetworkLevel", SendMessageOptions.DontRequireReceiver);
+	 		}
+		 	
+		 }
+		 
+		 if(GUILayout.Button ("START SERVER")){
+		 for (var i : GameObject in FindObjectsOfType(GameObject)){
+	 			i.BroadcastMessage("OnLoadNetworkLevel", SendMessageOptions.DontRequireReceiver);
+	 		}
+		 print ("starting server on " + ipAddress + ":" + port.ToString());
+		 Network.InitializeServer (maxConnections, port);
+		 
+	 	}
+	 	if(GUILayout.Button("MAIN MENU")){
+	 		Application.LoadLevel("Opening");
+	 	}
+	 }
+	 	else {
+	 		if(GUILayout.Button("DISCONNECT")){ 		
+	 		Network.Disconnect(200);
+	 		}
+	 		if (Input.GetKeyDown (KeyCode.T)){
+	 		chatting = true;
+	 }
+	 }
+	 		if (Input.GetKeyDown (KeyCode.Escape)){
+	 		chatting = false;
+	 }
+	 if (chatting){
+	 GUILayout.Label(log);
+	 myMessage = GUILayout.TextField(myMessage);
 	 
-	 GUILayout.BeginHorizontal();
-	 var tempPort : String; 
-	 tempPort = GUILayout.TextField(port.ToString());
-	 port = parseInt (tempPort);
-	 GUILayout.Label ("PORT");
-	 GUILayout.EndHorizontal();
-	 
-	 if (GUILayout.Button ("CONNECT")) {
-	 
-	 	print ("connecting...");
-	  	Network.Instantiate (playerGO, transform.position, transform.rotation, 0);
-	 	Network.Connect (ipAddress, port);
-	 	for (var i : GameObject in FindObjectsOfType(GameObject)){
- 			i.BroadcastMessage("OnLoadNetworkLevel", SendMessageOptions.DontRequireReceiver);
- 		}
-	 	
+	 if(GUILayout.Button("SEND MESSAGE")){
+	 	networkView.RPC("Chat", RPCMode.All, myMessage);
+	 	}
 	 }
 	 
-	 if(GUILayout.Button ("START SERVER")){
-	 for (var i : GameObject in FindObjectsOfType(GameObject)){
- 			i.BroadcastMessage("OnLoadNetworkLevel", SendMessageOptions.DontRequireReceiver);
- 		}
-	 print ("starting server on " + ipAddress + ":" + port.ToString());
-	 Network.InitializeServer (maxConnections, port);
 	 
- 	}
- }
- 	else {
- 		if(GUILayout.Button("DISCONNECT")){ 		
- 		Network.Disconnect(200);
- 		}
- 		if (Input.GetKeyDown (KeyCode.T)){
- 		chatting = true;
- }
- }
- 		if (Input.GetKeyDown (KeyCode.Escape)){
- 		chatting = false;
- }
- if (chatting){
- GUILayout.Label(log);
- myMessage = GUILayout.TextField(myMessage);
- 
- if(GUILayout.Button("SEND MESSAGE")){
- networkView.RPC("Chat", RPCMode.All, myMessage);
- }
- }
- 
- 
  }
 
 
