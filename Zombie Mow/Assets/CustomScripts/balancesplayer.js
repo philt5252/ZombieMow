512f6f3f307fecff2033a2473e940b3f3c95f683
Ammie <knapiflable@hotmail.com>
2014-02-24 10:24:10 -0600

    super update
    with teh first person controller

--- Zombie Mow/Assets/CustomScripts/balancesplayer.js
+++ Zombie Mow/Assets/CustomScripts/balancesplayer.js
@@ -1,11 +1,11 @@
 ﻿function Update () {
 
 	if(!networkView.isMine) {
-	gameObject.GetComponent(ThirdPersonController).enabled = false;
-	gameObject.GetComponent(ThirdPersonCamera).enabled = false;
-//	GetComponent(MouseLook).enabled = false;
+	GetComponent(CharacterMotor).enabled = false;
+	GetComponent(FPSInputController).enabled = false;
+	GetComponent(MouseLook).enabled = false;
 	
-//	this.gameObject.BroadcastMessage("DisableCamera");
+	this.gameObject.BroadcastMessage("DisableCamera");
 
 }
 
