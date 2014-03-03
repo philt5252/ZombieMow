da3a052a554c77733d32536777deec8e9acad4bf
Ammie <knapiflable@hotmail.com>
2014-03-03 11:09:49 -0600

    health bar ui update again

--- ArtAssets/3d ArenaModel/ZombiMowTest/Assets/scripts/Health.js
+++ ArtAssets/3d ArenaModel/ZombiMowTest/Assets/scripts/Health.js
@@ -2,6 +2,8 @@
  
 var healthBG : Texture2D;
 var healthFG : Texture2D;
+var healthBorder : Texture2D;
+var bulletShot : GameObject;
 var healthFGMaxWidth : float; //the starting width of the foreground bar
 var currentHealth:float;
  
@@ -12,12 +14,20 @@ function Start()
     currentHealth = 100;
 }
  
-function Update () 
+//function Update () 
+//{
+//    if (Collision == bulletShot.collider)
+//       currentHealth -= 1;
+//}
+function OnCollisionEnter(collision : Collision)
 {
-    if (Input.GetButtonDown("Fire1"))
-       currentHealth -= 1;
+	if(collision.gameObject.tag == "Respawn")
+	{
+		currentHealth -= 1;
+		return currentHealth;
+	}
 }
- 
+
 function OnGUI()
 {
     //this is the width that the foreground bar should be
@@ -27,13 +37,21 @@ function OnGUI()
  
     //a spacing variable to help us position the health
     var gap:int = 20;
- 
-    GUI.BeginGroup(new Rect (Screen.width - healthBG.width - gap, 
-       gap, healthBG.width, healthBG.height));
-       GUI.DrawTexture(Rect (0,0, healthBG.width, healthBG.height), healthBG);
- 
-       GUI.BeginGroup(new Rect(0,0, newBarWidth, healthFG.height));
-         GUI.DrawTexture(Rect(0,0, healthFG.width, healthFG.height), healthFG);
-       GUI.EndGroup();
-    GUI.EndGroup();
+    
+  GUI.BeginGroup(new Rect (Screen.width - healthBG.width - gap, 
+	       gap, healthBG.width, healthBG.height));
+	       GUI.DrawTexture(Rect (0,0, healthBG.width, healthBG.height), healthBG);
+	 	
+	       GUI.BeginGroup(new Rect(0,0,healthBG.width,healthBorder.height)); 
+					GUI.DrawTexture(Rect(0,0,healthBorder.width,healthBorder.height), healthBorder);
+	       
+	       GUI.BeginGroup(new Rect(0,0, newBarWidth, healthFG.height));
+	         GUI.DrawTexture(Rect(0,0, healthFG.width, healthFG.height), healthFG);
+	        	
+	        	  GUI.BeginGroup(new Rect(0,0,healthBG.width,healthBorder.height)); 
+					GUI.DrawTexture(Rect(0,0,healthBorder.width,healthBorder.height), healthBorder);
+	         
+	       GUI.EndGroup();
+	 	 GUI.EndGroup();
+	GUI.EndGroup();
 }
