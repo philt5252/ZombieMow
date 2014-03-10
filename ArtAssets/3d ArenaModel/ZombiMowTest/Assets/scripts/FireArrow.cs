adcc64941834aaf23461cc137219e03f04f7ca44
Ammie <knapiflable@hotmail.com>
2014-03-10 10:48:52 -0500

    bwwwaaaaahhhhhhhh
    rpc update thingy

--- ArtAssets/3d ArenaModel/ZombiMowTest/Assets/scripts/FireArrow.cs
+++ ArtAssets/3d ArenaModel/ZombiMowTest/Assets/scripts/FireArrow.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-[RPC]
+
 public class FireArrow : MonoBehaviour
 {
 	public Rigidbody arrowPrefab;
@@ -11,26 +11,21 @@ public class FireArrow : MonoBehaviour
 
 	private bool powerUp = false;
 	
-	//[RPC]
+
 	void Update ()
 	{
 		if(Input.GetButtonDown("Fire1"))
 		{
-			if(powerUp)
-			{
-				Rigidbody fireInstance;
-				fireInstance = Instantiate(firePrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
-				fireInstance.AddForce(barrelEnd.forward * 5000);
-				audio.PlayOneShot(fireShot);
-			}
-
-			else
-			{
-				Rigidbody arrowInstance;
-				arrowInstance = Instantiate(arrowPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
-				arrowInstance.AddForce(barrelEnd.forward * 5000);
-				audio.PlayOneShot(arrowShot);
-			}
+			CreateArrow();
+		
 		}
+	}
+	[RPC]
+	void CreateArrow()
+	{
+		Rigidbody arrowInstance;
+		arrowInstance = Instantiate(arrowPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+		arrowInstance.AddForce(barrelEnd.forward * 5000);
+		audio.PlayOneShot(arrowShot);
 	}
 }
