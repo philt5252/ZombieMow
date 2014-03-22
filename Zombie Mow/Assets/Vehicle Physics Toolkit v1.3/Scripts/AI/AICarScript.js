f128e1eaf30b12f5a6eec6f67f5df86c6cf366fb
philt5252 <philt5252@gmail.com>
2014-03-22 16:53:47 -0500

    much car

---
+++ Zombie Mow/Assets/Vehicle Physics Toolkit v1.3/Scripts/AI/AICarScript.js
@@ -0,0 +1,116 @@
+@script AddComponentMenu ("CarPhys/AIScripts/AICarScript")
+var centerOfMass : Vector3;  
+var path : Array;  
+var pathGroup : Transform;  
+var maxSteer : float = 15.0;  
+var wheelFL : WheelCollider;   
+var wheelFR : WheelCollider;  
+var wheelRL : WheelCollider;   
+var wheelRR : WheelCollider;  
+var currentPathObj : int;  
+var distFromPath : float = 20;  
+var maxTorque : float = 50;  
+var currentSpeed : float;  
+var topSpeed : float = 150;  
+var decellarationSpeed : float = 10;  
+//var gearRatio : int[];	
+var breakingMesh : Renderer;  
+var idleBreakLight : Material;  
+var activeBreakLight : Material;  
+var isBreaking : boolean;  
+var inSector : boolean;  
+var isControll = false;
+  
+function Start () {  
+rigidbody.centerOfMass = centerOfMass;  
+GetPath();  
+}  
+  
+function GetPath (){  
+var path_objs : Array = pathGroup.GetComponentsInChildren(Transform);  
+path = new Array();  
+  
+for (var path_obj : Transform in path_objs){  
+/*if(path_obj.parent == pathGroup.parent)*/
+  path.Add(path_obj);  
+}  
+}  
+/*  
+function EngineSound(){
+for (var i = 0; i < gearRatio.length; i++){
+if(gearRatio[i]> currentSpeed){
+break;
+}
+}
+var gearMinValue : float = 0.00;
+var gearMaxValue : float = 0.00;
+if (i == 0){
+gearMinValue = 0;
+}
+else {
+gearMinValue = gearRatio[i-1];
+}
+gearMaxValue = gearRatio[i];
+var enginePitch : float = ((currentSpeed - gearMinValue)/(gearMaxValue - gearMinValue))+1;
+audio.pitch = enginePitch;
+}
+*/
+    
+      
+function Update () {  
+GetSteer(); 
+if(isControll){
+Move(); 
+
+}
+BreakingEffect ();  
+
+}  
+ 
+function FixedUpdate () {
+//rigidbody.AddForce (0, -100, 0);
+}  
+function GetSteer(){  
+var inUse: Transform = path[currentPathObj]as Transform;
+
+var steerVector : Vector3 = transform.InverseTransformPoint(Vector3(inUse.position.x, inUse.position.y,inUse.position.z));
+var newSteer : float = maxSteer * (steerVector.x / steerVector.magnitude);  
+wheelFL.steerAngle = newSteer;  
+wheelFR.steerAngle = newSteer;  
+
+if (steerVector.magnitude <= distFromPath){  
+currentPathObj++;
+}  
+if (currentPathObj >= path.length ){  
+currentPathObj = 0; 
+} 
+
+  
+}  
+  
+function Move (){  
+currentSpeed = 2*(22/7)*wheelRL.radius*wheelRL.rpm * 60 / 1000;  
+currentSpeed = Mathf.Round (currentSpeed);  
+if (currentSpeed <= topSpeed && !inSector){  
+wheelRL.motorTorque = maxTorque;  
+wheelRR.motorTorque = maxTorque;  
+wheelRL.brakeTorque = 0;  
+wheelRR.brakeTorque = 0;  
+}  
+else if (!inSector){  
+wheelRL.motorTorque = 0;  
+wheelRR.motorTorque = 0;  
+wheelRL.brakeTorque = decellarationSpeed;  
+wheelRR.brakeTorque = decellarationSpeed;  
+}  
+}  
+  
+function BreakingEffect (){  
+if (isBreaking){  
+breakingMesh.material = activeBreakLight;  
+}  
+else {  
+breakingMesh.material = idleBreakLight;  
+}  
+  
+}   
---
+++ Zombie Mow/Assets/Vehicle Physics Toolkit v1.3/Scripts/Player/CarControlNitro.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+@script AddComponentMenu ("CarPhys/Scripts/CarControleNitro")
+private var nitro = false;
+private var currentTorque = 0.0f;
+var maxNitroAccelaration = 50;
+var maxTorque = 20;
+var nitroVolume = 80f;
+var maxNitrovolume = 100;
+var carTorque = 0;
+var particleEffectLeft : GameObject;
+var particleEffectRight : GameObject;
+var guiSkin : GUISkin;
+
+function Start(){
+particleEffectLeft.GetComponent(ParticleEmitter).emit = false;
+particleEffectRight.GetComponent(ParticleEmitter).emit = false;
+particleEffectLeft.GetComponent(LensFlare).enabled = false;
+particleEffectRight.GetComponent(LensFlare).enabled = false;
+}
+
+function Update () {
+carTorque = gameObject.GetComponent(CarControleScript).maxTorque = currentTorque;
+
+Nitro();
+
+if(nitroVolume<=0){
+nitro = false;
+nitroVolume = 0;
+currentTorque = maxTorque;
+particleEffectLeft.GetComponent(ParticleEmitter).emit = false;
+particleEffectRight.GetComponent(ParticleEmitter).emit = false;
+particleEffectLeft.GetComponent(LensFlare).enabled = false;
+particleEffectRight.GetComponent(LensFlare).enabled = false;
+}
+if(nitroVolume> maxNitrovolume){
+nitroVolume = maxNitrovolume;
+}
+
+
+if(Input.GetKey(KeyCode.Z)){
+nitro = true;
+}else{
+nitro = false;
+}
+}
+
+function Nitro(){
+if(nitro){
+currentTorque = maxNitroAccelaration;
+nitroVolume -= 1;
+particleEffectLeft.GetComponent(ParticleEmitter).emit = true;
+particleEffectRight.GetComponent(ParticleEmitter).emit = true;
+particleEffectRight.GetComponent(LensFlare).enabled = true;
+particleEffectLeft.GetComponent(LensFlare).enabled = true;
+}
+if(!nitro){
+currentTorque = maxTorque;
+nitroVolume += 1;
+particleEffectLeft.GetComponent(ParticleEmitter).emit = false;
+particleEffectRight.GetComponent(ParticleEmitter).emit = false;
+particleEffectLeft.GetComponent(LensFlare).enabled = false;
+particleEffectRight.GetComponent(LensFlare).enabled = false;
+}
+}
+
+
+function OnGUI(){
+GUI.skin = guiSkin;
+
+GUI.Label(Rect (100,50,300,100),"Current torque : " + currentTorque );
+GUI.Label(Rect (100,80,300,100),"Nitro Volume : " + nitroVolume);
+}
+
+
+
+
