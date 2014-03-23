6680503feea3827bf541072e70d9f0ba37ce8d6a
philt5252 <philt5252@gmail.com>
2014-03-22 23:21:37 -0500

    changin thangs!

--- Zombie Mow/Assets/CustomScripts/balancesplayer.js
+++ Zombie Mow/Assets/CustomScripts/balancesplayer.js
@@ -1,13 +1,30 @@
-﻿function Update () {
+﻿var location : Vector3;
+var newGameObject : GameObject;
 
-	if(!networkView.isMine) {
-	GetComponent(CarControleScript).enabled = false;
-	GetComponent(CarControlNitro).enabled = false;
-	//GetComponent(MouseLook).enabled = false;
+function Update () {
+
+	if(!networkView.isMine) 
+	{
 	
+		GetComponent(CarControleScript).enabled = false;
+		GetComponent(CarControlNitro).enabled = false;
 	
-	this.gameObject.BroadcastMessage("DisableCamera");
-
-}
-
+		this.gameObject.BroadcastMessage("DisableCamera");
+	}
+	else
+	{
+		if(this.gameObject.rigidbody.position.y < 10)
+		{
+			//Destroy(gameObject);
+			//GetComponent(Camera).rect = Rect ( 0.7, 0.7, 0.25, 0.25);
+			//GetComponent(GUILayer).enabled = false;
+			//Instantiate(newGameObject, location, Quaternion.identity);
+			
+			this.transform.position = location;
+			this.transform.rotation = Quaternion.identity;
+			this.rigidbody.velocity = Vector3.zero;
+			this.rigidbody.angularVelocity = Vector3.zero;
+			//Network.Instantiate(newGameObject, location, Quaternion.identity, 0);
+		}
+	}
 }
---
+++ Zombie Mow/Assets/RandSpawnScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandSpawnScript : MonoBehaviour {
+
+	private float elapsedTime = 0;
+	private float spawnSeconds = 5;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		elapsedTime += Time.deltaTime;
+
+		if (elapsedTime > spawnSeconds*1000) 
+		{
+
+		}
+		if (Network.connections.Length > 0) 
+		{
+
+		}
+	}
+}
